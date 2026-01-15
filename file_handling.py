9bae2b3057d22c33534e3cac4bf4dc5f9482284b
vaibhavhaval05 <havalvaibhav19@gmail.com>
2026-01-16 01:18:04 +1300

    Add files via upload

---
+++ file_handling.py
@@ -0,0 +1,6 @@
+file = open("myfile.txt","w+")
+print("open file",file.name)
+print("readable",file.readable())
+print("writable",file.writable())
+print("file mode",file.mode)
+print("file close",file.close())
