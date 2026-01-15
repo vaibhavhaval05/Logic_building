bf7f5b9b076a5ecae71966f177ab6e37fa16e546
vaibhavhaval05 <havalvaibhav19@gmail.com>
2026-01-15 20:03:04 +1300

    Add files via upload

---
+++ exception2.py
@@ -0,0 +1,13 @@
+try :
+    a = int(input("enter first number:"))
+    b = int(input("enter second number:"))
+    print(a/b)
+
+except(ZeroDivisionError,ValueError):
+    print("enter proper value")
+
+else:
+    print("every thing is perfect")
+
+finally:
+    print("I'll execute every time")
