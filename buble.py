1c192677c89d11a5b68a0ddefa02a97eb59b757f
Vaibhav haval <havalvaibhav19@gmail.com>
2026-02-07 03:55:00 +1300

    Fix formatting and add newline at end of buble.py
    Added newline at the end of the file and corrected formatting.

--- buble.py
+++ buble.py
@@ -8,4 +8,5 @@ for i in range(n-1):
     for j in range(n-i-1):
         if my_array[j]>= my_array[j+1]:
             my_array[j],my_array[j+1]=my_array[j+1],my_array[j]
+
 print("sorted numbers",my_array)
